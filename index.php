51d0b0e1fba8bca26b04829ff3e060f33574fbfc
Dog-Matix <Dog-Matix@users.noreply.github.com>
2017-04-19 17:52:31 +0100

    Right Hand Column Sidebar added
    Right Hand Column Sidebar added

--- index.php
+++ index.php
@@ -6,11 +6,9 @@
  * @link http://apdickens.co.uk/
  * @license GNU General Public License version 2 or later; see LICENSE.txt
  **/
-
     // Loading Bootstrap 3.3.7
     JHtml::stylesheet(Juri::base() . 'templates/biginstitute/css/jui/bootstrap.min.css');
     JHtml::script(Juri::base() . 'templates/biginstitute/js/jui/bootstrap.min.js', 'bootstrap', true, false, true, true);
-
     //No Direct Access
     defined('_JEXEC') or die ('Restricted access');
 ?>
@@ -19,6 +17,10 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
     
     <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
 
         <jdoc:include type="head" />
 
@@ -29,16 +31,26 @@
 
     <body>
 
-		
+        
         <jdoc:include type="modules" name="top" />
 
         <jdoc:include type="modules" name="showcase" />
 
-        <div data-spy="affix" data-offset-top="396"><jdoc:include type="modules" name="navigation" /></div>
+        <nav data-spy="affix" data-offset-top="396"><jdoc:include type="modules" name="navigation" /></nav>
 
-		<div class="container">
-        	<jdoc:include type="component" />
-      	</div>
+        <div class="container">
+            <div style="row">
+
+                <div style="col-md-8">
+                    <jdoc:include type="component" />
+                </div>
+
+                <div style="col-md-4">
+                    <jdoc:include type="modules" name="sidebar" />
+                </div>
+
+            </div>
+        </div>
       
         <jdoc:include type="modules" name="bottom" />
 
@@ -46,7 +58,7 @@
 
 
     <!-- Load JQuery -->
-    <script defer src="templates/<?php echo $this->template ?>/js/jquery.min.js"></script>
+    <script defer src="templates/<?php echo $this->template ?>/js/jquery.js"></script>
 
     <!-- Load Custom JS -->
     <script defer src="templates/<?php echo $this->template ?>/js/biginstitute.js"></script>
