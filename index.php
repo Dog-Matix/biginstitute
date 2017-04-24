959bb8af92a70d3ca2efa82a72919ca1cf872d40
Dog-Matix <Dog-Matix@users.noreply.github.com>
2017-04-24 17:56:50 +0100

    New Positions Added
    3 column spotlight row added. Footer added with 4 positions.

--- index.php
+++ index.php
@@ -38,9 +38,9 @@
             <nav data-spy="affix" data-offset-top="444"><jdoc:include type="modules" name="navigation" /></nav>
         </div>
 
-    <div class="container">
-      <div class="row">
-        <?php echo (isset($sidebar) ? '<div class="col-md-8">' : '<div class="col-md-12">'); ?>
+    <div class="container"><!-- container 1 -->
+        <div class="row">
+        <?php echo (isset($sidebar) ? '<div class="col-md-8 col-sm-8">' : '<div class="col-md-12">'); ?>
             <!-- Messages -->
             <jdoc:include type="message" />
             <?php if($show_frontpage_component == 0 && $menu->getActive() == $menu->getDefault()) : ?>
@@ -57,13 +57,78 @@
                 <jdoc:include type="modules" name="sidebar" style="xhtml" />
             </div>
         <?php endif; ?>
-    </div><!--./ row -->
+        </div><!--./ row -->
+
+    </div><!-- /.container 1 -->
+
+
+    <div class="spotlights">
+        <div class="container"><!-- container 2 -->
+        <?php if($this->countModules('spotlight1') || $this->countModules('spotlight2') || $this->countModules('spotlight3')) : ?>
+            <!-- Example row of columns -->
+            <div class="row">
+            <?php if($this->countModules('spotlight1')) : ?>
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <jdoc:include type="modules" name="spotlight1" style="none" />
+                </div>
+            <?php endif; ?>
+            <?php if($this->countModules('spotlight2')) : ?>
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <jdoc:include type="modules" name="spotlight2" style="none" />
+                </div>
+            <?php endif; ?>
+            <?php if($this->countModules('spotlight3')) : ?>
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <jdoc:include type="modules" name="spotlight3" style="none" />
+                </div>
+            <?php endif; ?>
+            </div>
+        <?php endif; ?>
+        </div><!-- /. container 2 -->
+    </div><!-- /.spotlights -->
+
+    <div class="bottom">       
+            <?php if($this->countModules('bottom')) : ?>
+                <jdoc:include type="modules" name="bottom" />
+            <?php endif; ?>
+    </div><!-- /.bottom -->
+
+    <div class="footer">
+        <div class="container"><!-- container 3 -->
+        <?php if($this->countModules('footer1') || $this->countModules('footer2') || $this->countModules('footer3')) : ?>
+            <!-- Example row of columns -->
+            <div class="row">
+            <?php if($this->countModules('footer1')) : ?>
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <jdoc:include type="modules" name="footer1" style="xhtml" />
+                </div>
+            <?php endif; ?>
+            <?php if($this->countModules('footer2')) : ?>
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <jdoc:include type="modules" name="footer2" style="xhtml" />
+                </div>
+            <?php endif; ?>
+            <?php if($this->countModules('footer3')) : ?>
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <jdoc:include type="modules" name="footer3" style="xhtml" />
+                </div>
+            <?php endif; ?>
+            </div>
+        <?php endif; ?>
       
-        <?php if($this->countModules('bottom')) : ?>
-            <jdoc:include type="modules" name="bottom" />
+        <?php if($this->countModules('footer4')) : ?>
+            <div class="col-md-12">
+                <jdoc:include type="modules" name="footer4" style="xhtml" />
+            </div>
         <?php endif; ?>
 
-        <jdoc:include type="modules" name="debug" />
+        </div><!-- /. container 3 -->
+    </div><!-- /.footer -->
+
+    <jdoc:include type="modules" name="debug" />
+
+    
+
 
 
     <!-- Load JQuery -->
