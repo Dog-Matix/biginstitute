88eed9b217d342db8c4c2b93eca0c88098f27bda
Dog-Matix <Dog-Matix@users.noreply.github.com>
2017-04-28 17:19:33 +0100

    Scroll spy and new module positions added
    New module positions. Scroll spy added. Rewrite of index.php as unused
    module positions were not hiding properly.

--- index.php
+++ index.php
@@ -22,9 +22,12 @@
 
         <!-- Load Custom CSS -->
         <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/biginstitute.css" type="text/css" />
+
+
+
     </head>
 
-    <body>
+    <body id="page-top" data-spy="scroll" data-target=".navbar">
         <?php if($this->countModules('top')) : ?>
             <jdoc:include type="modules" name="top" style="none" />
         <?php endif; ?>
@@ -60,54 +63,146 @@
 
     </div><!-- /.container 1 -->
 
+    <?php if($this->countModules('UpperFullWidth') || $this->countModules('UpperColLeft') || $this->countModules('UpperColRight')) : ?>
+        <div id="UpperFullWidth" class="content">
+            <div class="container"><!-- container 2 -->
 
-    <div class="content">
-        <div class="container"><!-- container 2 -->
+                <?php if ($this->countModules('UpperFullWidth')) : ?>
+                    <div class="row">
+                        <div class="col-md-12 col-xs-12">
+                            <jdoc:include type="modules" name="UpperFullWidth" style="xhtml" />
+                        </div><!-- /.columnLeft -->
+                    </div><!-- /.row -->
+                <?php endif; ?>
 
-            <?php if ($this->countModules('fullWidth')) : ?>
-                <div class="col-md-12 col-xs-12">
-                    <jdoc:include type="modules" name="fullWidth" style="xhtml" />
-                </div><!-- /.columnLeft -->
-            <?php endif; ?>
-            <?php if ($this->countModules('columnLeft')) : ?>
-                <div class="col-md-6 col-xs-12">
-                    <jdoc:include type="modules" name="columnLeft" style="xhtml" />
-                </div><!-- /.columnLeft -->
-            <?php endif; ?>
-            <?php if ($this->countModules('columnRight')) : ?>    
-                <div class="col-md-6 col-xs-12">
-                    <jdoc:include type="modules" name="columnRight" style="xhtml" />
-                </div><!-- /.columnRight -->
-            <?php endif; ?> 
+                <div class="row">
+                    <?php if ($this->countModules('UpperColLeft')) : ?>
+                        <div class="col-md-6 col-xs-12">
+                            <jdoc:include type="modules" name="UpperColLeft" style="xhtml" />
+                        </div><!-- /.columnLeft -->
+                    <?php endif; ?>
+                    <?php if ($this->countModules('UpperColRight')) : ?>    
+                        <div class="col-md-6 col-xs-12">
+                            <jdoc:include type="modules" name="UpperColRight" style="xhtml" />
+                        </div><!-- /.columnRight -->
+                    <?php endif; ?>
+                </div><!-- /.row -->
+                
+            </div><!-- /.container 2 -->
+        </div><!-- /.content -->
+    <?php endif; ?>
+
+    <?php if($this->countModules('spotlight1') || $this->countModules('spotlight2') || $this->countModules('spotlight3')) : ?>
+        <div class="spotlights">
+            <div class="container"><!-- container 3 -->
             
-        </div><!-- /.container 2 -->
+                <!-- Example row of columns -->
+                <div class="row">
+                    <?php if($this->countModules('spotlight1')) : ?>
+                        <div class="col-md-4 col-sm-4 col-xs-12">
+                            <jdoc:include type="modules" name="spotlight1" style="none" />
+                        </div>
+                    <?php endif; ?>
+                    <?php if($this->countModules('spotlight2')) : ?>
+                        <div class="col-md-4 col-sm-4 col-xs-12">
+                            <jdoc:include type="modules" name="spotlight2" style="none" />
+                        </div>
+                    <?php endif; ?>
+                    <?php if($this->countModules('spotlight3')) : ?>
+                        <div class="col-md-4 col-sm-4 col-xs-12">
+                            <jdoc:include type="modules" name="spotlight3" style="none" />
+                        </div>
+                    <?php endif; ?>
+                </div><!-- /.row -->
+            
+            </div><!-- /. container 3 -->
+        </div><!-- /.spotlights1 -->
+    <?php endif; ?>
 
-    </div><!-- /.content -->
+    <?php if($this->countModules('MidFullWidth') || $this->countModules('MidColLeft') || $this->countModules('MidColRight')) : ?>
+        <div id="MidFullWidth" class="content">
+            <div class="container"><!-- container 4 -->
 
-    <div class="spotlights">
-        <div class="container"><!-- container 3 -->
-        <?php if($this->countModules('spotlight1') || $this->countModules('spotlight2') || $this->countModules('spotlight3')) : ?>
-            <!-- Example row of columns -->
-            <div class="row">
-            <?php if($this->countModules('spotlight1')) : ?>
-                <div class="col-md-4 col-sm-4 col-xs-12">
-                    <jdoc:include type="modules" name="spotlight1" style="none" />
+                <?php if ($this->countModules('MidFullWidth')) : ?>
+                    <div class="row">
+                        <div class="col-md-12 col-xs-12">
+                            <jdoc:include type="modules" name="MidFullWidth" style="xhtml" />
+                        </div><!-- /.MidFullWidth -->
+                    </div>
+                <?php endif; ?>
+
+                <div class="row">
+                    <?php if ($this->countModules('MidColLeft')) : ?>
+                        <div class="col-md-6 col-xs-12">
+                            <jdoc:include type="modules" name="MidColLeft" style="xhtml" />
+                        </div><!-- /.columnLeft -->
+                    <?php endif; ?>
+                    <?php if ($this->countModules('MidColRight')) : ?>    
+                        <div class="col-md-6 col-xs-12">
+                            <jdoc:include type="modules" name="MidColRight" style="xhtml" />
+                        </div><!-- /.columnRight -->
+                    <?php endif; ?>
+                </div><!-- /.row -->
+
+            </div><!-- /.container 4 -->
+        </div><!-- /.content -->
+    <?php endif; ?>
+
+    <?php if($this->countModules('spotlight4') || $this->countModules('spotlight5') || $this->countModules('spotlight6')) : ?>
+        <div class="spotlights">
+            <div class="container"><!-- container 5 -->
+            
+                <!-- Example row of columns -->
+                <div class="row">
+                <?php if($this->countModules('spotlight4')) : ?>
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <jdoc:include type="modules" name="spotlight4" style="none" />
+                    </div>
+                <?php endif; ?>
+                <?php if($this->countModules('spotlight5')) : ?>
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <jdoc:include type="modules" name="spotlight4" style="none" />
+                    </div>
+                <?php endif; ?>
+                <?php if($this->countModules('spotlight6')) : ?>
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <jdoc:include type="modules" name="spotlight6" style="none" />
+                    </div>
+                <?php endif; ?>
                 </div>
-            <?php endif; ?>
-            <?php if($this->countModules('spotlight2')) : ?>
-                <div class="col-md-4 col-sm-4 col-xs-12">
-                    <jdoc:include type="modules" name="spotlight2" style="none" />
-                </div>
-            <?php endif; ?>
-            <?php if($this->countModules('spotlight3')) : ?>
-                <div class="col-md-4 col-sm-4 col-xs-12">
-                    <jdoc:include type="modules" name="spotlight3" style="none" />
-                </div>
-            <?php endif; ?>
-            </div>
-        <?php endif; ?>
-        </div><!-- /. container 3 -->
-    </div><!-- /.spotlights -->
+
+            </div><!-- /. container 5 -->
+        </div><!-- /.spotlights2 -->
+    <?php endif; ?>
+
+    <?php if($this->countModules('LowerFullWidth') || $this->countModules('LowerColLeft') || $this->countModules('LowerColRight')) : ?>
+        <div id="LowerFullWidth" class="lowercontent">
+            <div class="container"><!-- container 6 -->
+
+                <?php if ($this->countModules('LowerFullWidth')) : ?>
+                    <div class="row">
+                        <div class="col-md-12 col-xs-12">
+                            <jdoc:include type="modules" name="LowerFullWidth" style="xhtml" />
+                        </div><!-- /.MidFullWidth -->
+                    </div>
+                <?php endif; ?>
+                
+                <div class="row">
+                    <?php if ($this->countModules('LowerColLeft')) : ?>
+                        <div class="col-md-6 col-xs-12">
+                            <jdoc:include type="modules" name="LowerColLeft" style="xhtml" />
+                        </div><!-- /.columnLeft -->
+                    <?php endif; ?>
+                    <?php if ($this->countModules('LowerColRight')) : ?>    
+                        <div class="col-md-6 col-xs-12">
+                            <jdoc:include type="modules" name="LowerColRight" style="xhtml" />
+                        </div><!-- /.columnRight -->
+                    <?php endif; ?>
+                </div><!-- /.row -->
+
+            </div><!-- /.container 6 -->
+        </div><!-- /.content -->
+    <?php endif; ?>
 
     <div class="bottom">       
             <?php if($this->countModules('bottom')) : ?>
@@ -116,7 +211,7 @@
     </div><!-- /.bottom -->
 
     <div class="footer">
-        <div class="container"><!-- container 4 -->
+        <div class="container"><!-- container 7 -->
         <?php if($this->countModules('footer1') || $this->countModules('footer2') || $this->countModules('footer3')) : ?>
             <!-- Example row of columns -->
             <div class="row">
@@ -144,14 +239,10 @@
             </div>
         <?php endif; ?>
 
-        </div><!-- /. container 4 -->
+        </div><!-- /. container 7 -->
     </div><!-- /.footer -->
 
     <jdoc:include type="modules" name="debug" />
-
-    
-
-
 
     <!-- Load JQuery -->
     <script defer src="templates/<?php echo $this->template ?>/js/jquery.min.js"></script>
