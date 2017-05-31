9b799570075cf71bc65fc55df1b70b3d0ef15c3f
Dog-Matix <Dog-Matix@users.noreply.github.com>
2017-05-31 18:39:03 +0100

    New Module Positions & Navbar fix
    Added new module positions and fixed an error that stopped Nav Position
    disappearing when not in use

--- index.php
+++ index.php
@@ -35,39 +35,76 @@
 
 
         <!--Navigation-->
-        <div class="nav-wrapper">    
-            <div data-spy="affix" data-offset-top="550" id="mainnav">
-                <div class="container">
-                    <div class="navbar navbar-default" role="navigation">
-                        
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#targetedestination">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
+        <?php if ($this->countModules('navigation')) : ?>
+            <div class="nav-wrapper">    
+                <div data-spy="affix" data-offset-top="550" id="mainnav">
+                    <div class="container">
+                        <div class="navbar navbar-default" role="navigation">
+                            
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#targetedestination">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
 
-                            <div id="brand">
-                                <a class="navbar-brand" href="#"><img src="images/BI_LOGO_50.png"></a>
+                                <div id="brand">
+                                    <a class="navbar-brand" href="#"><img src="images/BI_LOGO_50.png"></a>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="navbar-collapse collapse" id="targetedestination">
-                        <?php if ($this->countModules('navigation')) : ?>
-                            <nav class="navigation" role="navigation">
-                                <jdoc:include type="modules" name="navigation" style="none" />
-                            </nav>
-                        <?php endif; ?>
-                        </div>
+                            <div class="navbar-collapse collapse" id="targetedestination">
+                            
+                                <nav class="navigation" role="navigation">
+                                    <jdoc:include type="modules" name="navigation" style="none" />
+                                </nav>
+                            
+                            </div>
 
+                        </div>
                     </div>
-                </div>
-            </div> 
-        </div><!-- /.nav-wrapper -->      
+                </div> 
+            </div><!-- /.nav-wrapper -->
+        <?php endif; ?>      
         <!--Navigation-->
 
-        <div id="maincontent" class="container"><!-- container 1 -->
+
+        <?php if($this->countModules('topspotlight1') || $this->countModules('topspotlight2') || $this->countModules('topspotlight3') || $this->countModules('topspotlight4')) : ?>
+            <div class="topspotlights">
+                <div class="container"><!-- container 1 -->
+                
+                    <!-- Example row of columns -->
+                    <div class="row">
+                        <?php if($this->countModules('topspotlight1')) : ?>
+                            <div class="col-md-3 col-sm-3 col-xs-12">
+                                <jdoc:include type="modules" name="topspotlight1" style="none" />
+                            </div>
+                        <?php endif; ?>
+                        <?php if($this->countModules('topspotlight2')) : ?>
+                            <div class="col-md-3 col-sm-3 col-xs-12">
+                                <jdoc:include type="modules" name="topspotlight2" style="none" />
+                            </div>
+                        <?php endif; ?>
+                        <?php if($this->countModules('topspotlight3')) : ?>
+                            <div class="col-md-3 col-sm-3 col-xs-12">
+                                <jdoc:include type="modules" name="topspotlight3" style="none" />
+                            </div>
+                        <?php endif; ?>
+                        <?php if($this->countModules('topspotlight4')) : ?>
+                            <div class="col-md-3 col-sm-3 col-xs-12">
+                                <jdoc:include type="modules" name="topspotlight4" style="none" />
+                            </div>
+                        <?php endif; ?>
+
+                    </div><!-- /.row -->
+                
+                </div><!-- /. container 1 -->
+            </div><!-- /.topspotlights -->
+        <?php endif; ?>
+
+
+        <div id="maincontent" class="container"><!-- container 2 -->
             <div class="row">
             <?php echo (isset($sidebar) ? '<div class="col-md-8 col-sm-8">' : '<div class="col-md-12">'); ?>
                 <!-- Messages -->
@@ -88,11 +125,11 @@
             <?php endif; ?>
             </div><!--./ row -->
 
-        </div><!-- /.container 1 -->
+        </div><!-- /.container 2 -->
 
         <?php if($this->countModules('UpperFullWidth') || $this->countModules('UpperColLeft') || $this->countModules('UpperColRight')) : ?>
             <div id="UpperFullWidth" class="content">
-                <div class="container"><!-- container 2 -->
+                <div class="container"><!-- container 3 -->
 
                     <?php if ($this->countModules('UpperFullWidth')) : ?>
                         <div class="row">
@@ -115,13 +152,13 @@
                         <?php endif; ?>
                     </div><!-- /.row -->
                     
-                </div><!-- /.container 2 -->
+                </div><!-- /.container 3 -->
             </div><!-- /.content -->
         <?php endif; ?>
 
         <?php if($this->countModules('spotlight1') || $this->countModules('spotlight2') || $this->countModules('spotlight3')) : ?>
             <div class="spotlights">
-                <div class="container"><!-- container 3 -->
+                <div class="container"><!-- container 4 -->
                 
                     <!-- Example row of columns -->
                     <div class="row">
@@ -142,13 +179,13 @@
                         <?php endif; ?>
                     </div><!-- /.row -->
                 
-                </div><!-- /. container 3 -->
+                </div><!-- /. container 4 -->
             </div><!-- /.spotlights1 -->
         <?php endif; ?>
 
         <?php if($this->countModules('MidFullWidth') || $this->countModules('MidColLeft') || $this->countModules('MidColRight')) : ?>
             <div id="MidFullWidth" class="midcontent">
-                <div class="container"><!-- container 4 -->
+                <div class="container"><!-- container 5 -->
 
                     <?php if ($this->countModules('MidFullWidth')) : ?>
                         <div class="row">
@@ -172,13 +209,13 @@
                         <?php endif; ?>
                     </div><!-- /.row -->
 
-                </div><!-- /.container 4 -->
+                </div><!-- /.container 5 -->
             </div><!-- /.content -->
         <?php endif; ?>
 
         <?php if($this->countModules('spotlight4') || $this->countModules('spotlight5') || $this->countModules('spotlight6')) : ?>
             <div class="spotlights">
-                <div class="container"><!-- container 5 -->
+                <div class="container"><!-- container 6 -->
                 
                     <!-- Example row of columns -->
                     <div class="row">
@@ -199,13 +236,13 @@
                     <?php endif; ?>
                     </div>
 
-                </div><!-- /. container 5 -->
+                </div><!-- /. container 6 -->
             </div><!-- /.spotlights2 -->
         <?php endif; ?>
 
         <?php if($this->countModules('LowerFullWidth') || $this->countModules('LowerColLeft') || $this->countModules('LowerColRight')) : ?>
             <div id="LowerFullWidth" class="lowercontent">
-                <div class="container"><!-- container 6 -->
+                <div class="container"><!-- container 7 -->
 
                     <?php if ($this->countModules('LowerFullWidth')) : ?>
                         <div class="row">
@@ -228,7 +265,7 @@
                         <?php endif; ?>
                     </div><!-- /.row -->
 
-                </div><!-- /.container 6 -->
+                </div><!-- /.container 7 -->
             </div><!-- /.content -->
         <?php endif; ?>
 
@@ -239,7 +276,7 @@
         <?php endif; ?>
 
         <div class="footer">
-            <div class="container"><!-- container 7 -->
+            <div class="container"><!-- container 8 -->
             <?php if($this->countModules('footer1') || $this->countModules('footer2') || $this->countModules('footer3')) : ?>
                 <!-- Example row of columns -->
                 <div class="row">
@@ -267,7 +304,7 @@
                 </div>
             <?php endif; ?>
 
-            </div><!-- /. container 7 -->
+            </div><!-- /. container 8 -->
         </div><!-- /.footer -->
 
         <jdoc:include type="modules" name="debug" />
